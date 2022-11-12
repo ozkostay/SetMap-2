78f160135bb1f6bd114b10d25bdc9913db02741b
ozkostay <ozkostay@mail.ru>
2022-11-12 13:01:11 +0500

    MAP metods -2

--- src/js/__tests__/errorRepository.test.js
+++ src/js/__tests__/errorRepository.test.js
@@ -5,4 +5,5 @@ test('Team add()', () => {
   errorsList.add(1, 'Ошибка 1');
   errorsList.add(2, 'Ошибка 2');
   expect(errorsList.translate(1)).toBe('Ошибка 1');
+  expect(errorsList.translate(3)).toBe('Unknown error');
 });
